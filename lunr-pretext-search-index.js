a30271683fb6a0b3ef1bbcd6c5cc6fe2564c5ddc
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-12-17 12:41:42 -0800

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -19,6 +19,33 @@ var ptx_lunr_docs = [
   "body": " Course Information      Course Title  Insert Course Title Here    Course Number  MTHXX    CRN  00000    Credits  0    Term and Year  Winter 2026    Prerequisites  MTH YY and ...    Class Meetings and Times  Mondays and Wednesdays 9:00AM - 11:50AM    Modality\/Location  In Person, SCOM 314, Southeast Campus       Course Description  Type your course description here. Course descriptions can be found at    Learning Outcomes  In this class, you will learn...  Outcome 1  Outcome 2      "
 },
 {
+  "id": "sec-instructor",
+  "level": "1",
+  "url": "sec-instructor.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Instructor Information",
+  "body": " Instructor Information      Instructor  your.email@pcc.edu    Phone  971-722-xxxx    Office Location  Building XXX    School Address  Portland Community College, 12000 SW 49th Ave., Portland, OR 97219       About Me  Write about yourself :)    Schedule   Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11      Office Hours           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11      "
+},
+{
+  "id": "sec-instructor-4-2",
+  "level": "2",
+  "url": "sec-instructor.html#sec-instructor-4-2",
+  "type": "Table",
+  "number": "2.1",
+  "title": "Term Schedule",
+  "body": " Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11    "
+},
+{
+  "id": "sec-instructor-4-3",
+  "level": "2",
+  "url": "sec-instructor.html#sec-instructor-4-3",
+  "type": "Table",
+  "number": "2.2",
+  "title": "Office Hours",
+  "body": " Office Hours           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11    "
+},
+{
   "id": "backmatter-2",
   "level": "1",
   "url": "backmatter-2.html",
