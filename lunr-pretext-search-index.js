c06e02e268dde211b45f6e4093c4d5059614474f
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2026-03-06 11:40:26 -0800

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1",
   "title": "Course Information",
-  "body": " Course Information      Course Title  Insert Course Title Here    Course Number  MTHXX    CRN  00000    Credits  0    Term and Year  Winter 2026    Prerequisites  MTH YY and ...    Class Meetings and Times  Mondays and Wednesdays 9:00AM - 11:50AM    Modality\/Location  In Person, SCOM 314, Southeast Campus       Course Description  Type your course description here. Course descriptions can be found at    Learning Outcomes  In this class, you will learn...  Outcome 1  Outcome 2      "
+  "body": " Course Information      Course Title  Insert Course Title Here    Course Number  MTHXX    CRN  00000    Credits  0    Term and Year  Winter 2026    Prerequisites  MTH YY and ...    Class Meetings and Times  Mondays and Wednesdays 9:00AM - 11:50AM    Modality\/Location  In Person, SCOM 314, Southeast Campus       Course Description   Type your course description here. Course descriptions can be found at     Learning Outcomes  In this class, you will learn...  Outcome 1  Outcome 2      "
 },
 {
   "id": "sec-instructor",
@@ -25,25 +25,88 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "2",
   "title": "Instructor Information",
-  "body": " Instructor Information      Instructor  your.email@pcc.edu    Phone  971-722-xxxx    Office Location  Building XXX    School Address  Portland Community College, 12000 SW 49th Ave., Portland, OR 97219       About Me  Write about yourself :)    Schedule   Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11      Office Hours           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11      "
+  "body": " Instructor Information      Instructor   your.email@pcc.edu     Phone  971-722-xxxx    Office Location  Building XXX    School Address  Portland Community College, 12000 SW 49th Ave., Portland, OR 97219       About Me  Write about yourself :)    Communication  The following guide will assist you in communicating with members of this course and outlines course-specific communication standards.   Best Way to Contact Me  Write about the best way to contact you.     Schedule  This term I am teaching the following classes:   Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final     class  crn  weekdays  start endtime  room  midterm  final      Office Hours       Day  Time  Place     Day  Time  Place      "
 },
 {
-  "id": "sec-instructor-4-2",
+  "id": "sec-instructor-5-3",
   "level": "2",
-  "url": "sec-instructor.html#sec-instructor-4-2",
+  "url": "sec-instructor.html#sec-instructor-5-3",
   "type": "Table",
   "number": "2.1",
   "title": "Term Schedule",
-  "body": " Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11    "
+  "body": " Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final     class  crn  weekdays  start endtime  room  midterm  final    "
 },
 {
-  "id": "sec-instructor-4-3",
+  "id": "sec-instructor-5-4",
   "level": "2",
-  "url": "sec-instructor.html#sec-instructor-4-3",
+  "url": "sec-instructor.html#sec-instructor-5-4",
   "type": "Table",
   "number": "2.2",
   "title": "Office Hours",
-  "body": " Office Hours           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11    "
+  "body": " Office Hours       Day  Time  Place     Day  Time  Place    "
+},
+{
+  "id": "sec-materials",
+  "level": "1",
+  "url": "sec-materials.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Materials",
+  "body": " Materials   Textbooks  What textbooks are required    Supplies  What supplies are required    Software\/Hardware\/Equipment  What software etc are required  Please visit the technology support section of Student Support Resources for a complete list of apps and tools for PCC students.    Technical Expecations  The technical requirements for the course include:    Technology Requirements for taking an online course at PCC      A recommended Internet browser installed      Ability to use Google Drive and Google Docs     Webcam and\/or microphone for meetings and online appointments       Accessibility  This course uses digital courseware, software, or texts that may cause barriers for those using assistive, or accessibility-related technologies. If you encounter barriers, please contact the Access Tech Team at access-tech-group@pcc.edu or by calling 971-722-8324.   "
+},
+{
+  "id": "sec-attendance",
+  "level": "1",
+  "url": "sec-attendance.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Attendance and Participation",
+  "body": " Attendance and Participation   Write your expectations for student participation here.    First Week Activities  During the first week of the term, instructors must:                     "
+},
+{
+  "id": "sec-grading",
+  "level": "1",
+  "url": "sec-grading.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Grading",
+  "body": " Grading   For more information, please go to the PCC Grading Guidelines .    Graded Assignments            Late Work and Make-Up Policy            Criteria  Please read over all assignments and discussion prompts in advance and reach out if you have any questions or if you want to confirm you understand them correctly. Clarifying questions will help you avoid deductions in your grades.       Activities  Number  Points Each  Total          Letter Grade  Points  Percentage    A   90-100%    B   80-89%    C   70-79%    D   60-69%    F   \\lt 59%      "
+},
+{
+  "id": "sec-policies",
+  "level": "1",
+  "url": "sec-policies.html",
+  "type": "Section",
+  "number": "6",
+  "title": "PCC Policies and Deadlines",
+  "body": " PCC Policies and Deadlines   Visit the PCC Policies page for information on:   Academic Integrity Policy    Accessibility Statement and Resources    Drop\/Withdraw Deadlines    Grading Policy    Internet Etiquette    PCC Payment Policy    PCC Registration Policy    Sanctuary College    Student Rights and Responsibilities    Title IX\/Non-Discrimination       Drop\/Withdraw Deadlines  A student is responsible to add, drop, or withdraw classes. Please review PCC Registration Policy for more information.   Students need to register online via MyPCC. Please review the Online Registration Instructions to find out how.    The last day to drop this class is Tuesday, Jan 13 . This is also the last day to register with instructor approval.    The last day to withraw is Saturday, March 14 .       Accomodations and Services  PCC is committed to supporting all students. If you plan to use academic accommodations for this course, please contact your instructor as soon as possible to discuss your needs. Accommodations are not retroactive; they begin when the instructor receives the “Approved Academic Accommodations” letter from you (submitted in person for courses on campus; via email for Online Learning courses). To request academic accommodations for a disability, please contact an Accessible Education and Disability Resources counselor on any PCC campus. Office locations, phone numbers, and additional information may be located on the Accessible Education and Disability Resources website .    Student Rights and Responsibilities  Students are required to complete this course in accordance with the Student Rights and Responsibilities Handbook . The Handbook establishes students' freedoms and protections as well as expectations of appropriate behavior and ethical academic work. The Handbook includes items such as the Policy on Student Rights, and the Student Code of Conduct Policy and Procedures.    Academic Integrity   Rules about cheating, plagiarism, or sharing work  The handbook contains the Code of Student Conduct and the Academic Integrity Policy. Cheating includes any attempt to defraud, deceive, or mislead the instructor in arriving at an honest grade assessment, and may include copying answers from other students or using unauthorized notes during tests. Plagiarism is a particular form of cheating that involves presenting as one's own the ideas or work of another and may include using other people's ideas without proper attribution and submitting another person's work as one's own. Dishonest activities such as cheating on exams and submitting or copying work done by others will result in disciplinary actions including but not limited to receiving a failing grade. For further information, review the institution's Academic Integrity Policy .    Generative AI  The use of generative AI tools, including but not limited to ChatGPT, to complete your work is strictly prohibited and will be considered equivalent to unauthorized sharing of work or plagiarism. All AI use is strongly discouraged, even as a study tool. I want to help you build a strong internal understanding of the material without relying on outside tools. If you have a question or need help, please reach out to me instead.  In return, I promise that I will not use generative AI in my relationship with you as your instructor. Any feedback you receive on your work will be my authentic human response to you.     Internet Etiquette  Some of this class may include online discussions or other interactions with students online. Click here for more information about Internet Etiquette (or Netiquette) .    Sanctuary College Statement  PCC is a sanctuary college. For more information and resources, see the resources for undocumented students page .    Inclement Weather Statement  Our course is typically unaffected by college campus and facility closures or delayed class start times. Our regular due dates apply. However, if PCC must close all operations, our regular due dates will be adjusted accordingly. Please check your email for my instructions and continue to check D2L Brightspace during closures.    Flexibility Statement  The instructor reserves the right to modify course content and\/or substitute assignments and learning activities in response to institutional, weather, or class situations.   "
+},
+{
+  "id": "sec-calendar",
+  "level": "1",
+  "url": "sec-calendar.html",
+  "type": "Section",
+  "number": "7",
+  "title": "Schedule",
+  "body": " Schedule   This schedule is a tentative outline and may change to meet the needs of the class.    Weekly Structure  Write about the weekly structure students can expect here.          Calendar   Course Calendar        Week  Monday  Wednesday  Assignments         date     date           Office Hours       Day  Time  Place     Day  Time  Place      "
+},
+{
+  "id": "sec-calendar-4-2",
+  "level": "2",
+  "url": "sec-calendar.html#sec-calendar-4-2",
+  "type": "Table",
+  "number": "7.1",
+  "title": "Course Calendar",
+  "body": " Course Calendar        Week  Monday  Wednesday  Assignments         date     date         "
+},
+{
+  "id": "sec-calendar-4-3",
+  "level": "2",
+  "url": "sec-calendar.html#sec-calendar-4-3",
+  "type": "Table",
+  "number": "7.2",
+  "title": "Office Hours",
+  "body": " Office Hours       Day  Time  Place     Day  Time  Place    "
 },
 {
   "id": "backmatter-2",
